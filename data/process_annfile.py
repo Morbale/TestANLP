eba65625ae5d20e61696c3aea7343c5dde0c9ab9
yoonichoi <yooni.jy.choi@gmail.com>
2023-10-12 22:58:55 -0400

    edited process_annfile to create aggregated conll file

--- data/process_annfile.py
+++ data/process_annfile.py
@@ -11,43 +11,37 @@ cntdic = {'MethodName':0,
           'DatasetName':0,
           'O':0}
 
-def process_conll(filepath, cnt):
-    with open(filepath, 'r') as input_file, open('temp.conll', 'w') as temp_file:
-        # Iterate through each line in the input file
-        for line in input_file:
-            # Check if the line contains "-DOCSTART- -X- O"
-            if "-DOCSTART- -X- O" not in line:
-                # Replace " -X- _ " with a space
-                modified_line = line.replace(" -X- _ ", " ")
-                # Write the modified line to the temporary file
-                temp_file.write(modified_line)
-                
-                # move on to the next line if the current line is empty
-                if cnt:
-                    if not modified_line.strip():
-                        continue
-                    
-                    tokens, named_entities = modified_line.strip().split()
-                    if named_entities != 'O':
-                        if named_entities[0] == 'B':
-                            cntdic[named_entities[2:]] += 1
-                    else:
-                        cntdic[named_entities] += 1
-    
-    # Rename the temporary file to overwrite the original input file
-    os.rename('temp.conll', filepath)
+def process_conll(folderpath, aggfile, cnt):
+    with open(aggfile, 'w') as aggfile:
+        for filename in glob.glob(os.path.join(folderpath, '*.conll')):
+            with open(filename, 'r') as input_file:
+                for line in input_file:
+                    if "-DOCSTART- -X- O" not in line:
+                        modified_line = line.replace(" -X- _ ", " ")
+                        aggfile.write(modified_line)
+                        
+                        if cnt:
+                            if not modified_line.strip():
+                                continue
+                            
+                            tokens, named_entities = modified_line.strip().split()
+                            if named_entities != 'O':
+                                if named_entities[0] == 'B':
+                                    cntdic[named_entities[2:]] += 1
+                            else:
+                                cntdic[named_entities] += 1
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--folder", type=str, help='folder containing conll files to be processed')
+    parser.add_argument("--aggfile", type=str, help='file path to aggregated conll file')
     parser.add_argument("--count", action='store_true', help='creates json of counts of each entity type')
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     args = get_args()
-    for file_path in glob.glob(os.path.join(args.folder, '*.conll')):
-        process_conll(file_path, args.count)
+    process_conll(args.folder, args.aggfile, args.count)
     if args.count:
         with open(os.path.join(args.folder,'annotation_cnt.json'), 'w') as json_file:
             json.dump(cntdic, json_file)
