91a826cc02822835344723b8bd3edd1a945b7b09
yoonichoi <yooni.jy.choi@gmail.com>
2023-10-16 11:34:57 -0400

    dataloader to train

--- dataloader.py
+++ dataloader.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import random
 from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
 
 # Create train, valid, test set from aggregated conll file
 def split_conll_file(args):
@@ -41,82 +42,118 @@ def split_conll_file(args):
         f.writelines(test_data)
 
 
+label2id = {'O': 0,
+            'B-MethodName':1,
+            'I-MethodName': 2,
+            'B-HyperparameterName': 3,
+            'I-HyperparameterName': 4,
+            'B-HyperparameterValue': 5,
+            'I-HyperparameterValue': 6,
+            'B-MetricName': 7,
+            'I-MetricName': 8,
+            'B-MetricValue': 9,
+            'I-MetricValue': 10,
+            'B-TaskName': 11,
+            'I-TaskName': 12,
+            'B-DatasetName': 13,
+            'I-DatasetName': 14
+            }
+
+def align_labels_with_tokens(labels, word_ids):
+    new_labels = []
+    current_word = None
+    for word_id in word_ids:
+        if word_id != current_word:
+            # Start of a new word!
+            current_word = word_id
+            label = -100 if word_id is None else labels[word_id]
+            new_labels.append(label)
+        elif word_id is None:
+            # Special token
+            new_labels.append(-100)
+        else:
+            # Same word as previous token
+            label = labels[word_id]
+            # If the label is B-XXX we change it to I-XXX
+            if label % 2 == 1:
+                label += 1
+            new_labels.append(label)
+
+    return new_labels
+
+def tokenize_and_align_labels(tokenizer, token, label):
+    tokenized_inputs = tokenizer(
+       token, is_split_into_words=True, 
+       return_offsets_mapping=True, 
+       padding='max_length',
+       max_length=512, 
+       truncation=False
+    )
+    labels = [label2id[l] for l in label]
+
+    word_ids = tokenized_inputs.word_ids()
+    new_labels=align_labels_with_tokens(labels, word_ids)
+
+    tokenized_inputs["labels"] = new_labels
+    return tokenized_inputs
+
+# read conll file, split by space and assign first to token, second to label
+def get_data(conll_file, model, batch_size):
+    token = []
+    label = []
+    with open(conll_file, 'r') as f:
+        data = f.readlines()
+        for line in data:
+            line = line.split()
+            if line:
+                token.append(line[0])
+                label.append(line[1])
+
+    # model = 'allenai/scibert_scivocab_cased'
+    tokenizer = AutoTokenizer.from_pretrained(model, pad_token_id=-100)
+
+    data = tokenize_and_align_labels(tokenizer, token, label)
+    input_id, label, attention_mask = data['input_ids'], data['labels'], data['attention_mask']
+
+    assert len(input_id) == len(label)
+
+    dataset = SciDataset(input_id, label, attention_mask)
+    
+    data_loader = DataLoader(dataset, batch_size=batch_size, collate_fn=dataset.collate_fn)
+    
+    return data_loader
+
+# to get token back: tokenizer.convert_ids_to_tokens(data['input_ids'])
 
 
-# label2id = {'O': 0,
-#             'B-MethodName':1,
-#             'I-MethodName': 2,
-#             'B-HyperparameterName': 3,
-#             'I-HyperparameterName': 4,
-#             'B-HyperparameterValue': 5,
-#             'I-HyperparameterValue': 6,
-#             'B-MetricName': 7,
-#             'I-MetricName': 8,
-#             'B-MetricValue': 9,
-#             'I-MetricValue': 10,
-#             'B-TaskName': 11,
-#             'I-TaskName': 12,
-#             'B-DatasetName': 13,
-#             'I-DatasetName': 14
-#             }
+class SciDataset(torch.utils.data.Dataset):
+    def __init__(self, input_ids, labels, attention_mask):
+        self.input_ids = input_ids
+        self.labels = labels
+        self.attention_mask = attention_mask
 
-# def align_labels_with_tokens(labels, word_ids):
-#     new_labels = []
-#     current_word = None
-#     for word_id in word_ids:
-#         if word_id != current_word:
-#             # Start of a new word!
-#             current_word = word_id
-#             label = -100 if word_id is None else labels[word_id]
-#             new_labels.append(label)
-#         elif word_id is None:
-#             # Special token
-#             new_labels.append(-100)
-#         else:
-#             # Same word as previous token
-#             label = labels[word_id]
-#             # If the label is B-XXX we change it to I-XXX
-#             if label % 2 == 1:
-#                 label += 1
-#             new_labels.append(label)
+    def __len__(self):
+        return len(self.input_ids)
 
-#     return new_labels
+    def __getitem__(self, idx):
+        # return in torch tensor
+        # make sure 2D tensor is getting returned
+        input_ids = torch.unsqueeze(torch.tensor(self.input_ids[idx]), 0)
+        labels = torch.unsqueeze(torch.tensor(self.labels[idx]), 0)
+        attention_mask = torch.unsqueeze(torch.tensor(self.attention_mask[idx]), 0)
+        return input_ids, labels, attention_mask
+        # return torch.tensor(self.input_ids[idx]), torch.tensor(self.labels[idx]), torch.tensor(self.attention_mask[idx])
 
-# def tokenize_and_align_labels(token, label):
-#     tokenized_inputs = tokenizer(
-#        token, is_split_into_words=True
-#     )
-#     labels = [label2id[l] for l in label]
-
-#     word_ids = tokenized_inputs.word_ids()
-#     new_labels=align_labels_with_tokens(labels, word_ids)
-
-#     tokenized_inputs["labels"] = new_labels
-#     return tokenized_inputs
-
-# # read conll file, split by space and assign first to token, second to label
-# token = []
-# label = []
-# with open(agg_conll, 'r') as f:
-#     data = f.readlines()
-#     for line in data:
-#         line = line.split()
-#         if line:
-#             token.append(line[0])
-#             label.append(line[1])
-
-# model = 'allenai/scibert_scivocab_cased'
-# tokenizer = AutoTokenizer.from_pretrained(model)
-# encode = tokenizer(token, is_split_into_words=True)
-
-# data = tokenize_and_align_labels(token, label)
-# label = torch.tensor(data['labels'])
-# input_id = torch.tensor(data['input_ids'])
-# # to get token back: tokenizer.convert_ids_to_tokens(data['input_ids'])
+    def collate_fn(self, batch):
+        # batch is a list of tuples of (input_ids, label)
+        # return in torch tensor
+        input_ids, labels, attention_mask = zip(*batch)
+        return torch.stack(input_ids, dim=0), torch.stack(labels, dim=0), torch.stack(attention_mask, dim=0)
 
 
 def get_args():
     parser = argparse.ArgumentParser()
+    # Args for splitting agg conll file
     parser.add_argument("--split", action='store_true', help='create data splits of train,val,test')
     parser.add_argument("--aggfile", type=str, help='aggregated data file path', default='data/aggdata.conll')
     parser.add_argument("--folder", type=str, help='destination folder to save split data, or split data to use', default='data/splits')
--- scibert_train.py
+++ scibert_train.py
@@ -1,16 +1,18 @@
 import argparse
 
 import torch
-from torch import optim, tensor
+from torch import optim
 from transformers import BertForTokenClassification
 from sklearn.metrics import precision_score, recall_score, f1_score
 
+from dataloader import *
 
-def train(args, inputs):
+
+def train(args, data_loader):
     num_labels = 15  # Define the number of unique labels for your specific task
     # Load the model
     model = BertForTokenClassification.from_pretrained(
-        'allenai/scibert_scivocab_cased', num_labels=15)
+        args.model, num_labels=15)
     # We already are getting the tokenized tokens
     # tokenizer = BertTokenizerFast.from_pretrained('allenai/scibert_scivocab_cased')
 
@@ -23,11 +25,14 @@ def train(args, inputs):
     if args.type == "finetune":
         for epoch in range(10):
             model.train()
-            for input_data in inputs:
+            for batch in data_loader:
+                input_ids = batch[0]
+                labels = batch[1]
+
                 optimizer.zero_grad()
-                outputs = model(**input_data)
+                outputs = model(input_ids, labels=labels)
                 loss = criterion(outputs.logits.view(-1, num_labels),
-                                 input_data['labels'].view(-1))  # Adjust labels accordingly
+                                 labels.view(-1))  # Adjust labels accordingly
                 loss.backward()
                 optimizer.step()
 
@@ -59,20 +64,29 @@ def evaluate(args, test):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, help='Model to use', default="allenai/scibert_scivocab_cased")
+    parser.add_argument("--batch_size", type=int, help='Batch Size', default=1)
+
     parser.add_argument("--optimizer", type=str, help='Adam or any other', default="Adam")
     parser.add_argument("--loss", type=str, help='Loss function to use', default="CrossEntropy")
     parser.add_argument("--epochs", type=int, help='Number of Epochs', default=10)
     parser.add_argument("--lr", type=float, help='Learning Rate', default=1e-3)
     parser.add_argument("--output_path", type=str, help='Path where model weights should be stored',
                         default="finetuned/")
+    
     parser.add_argument("--type", type=str, help='Freeze/Finetune', default="finetune")
     args = parser.parse_args()
-    #
-    ipt = [{'input_ids': tensor([[100, 864, 19676, 3832, 100, 173, 14203, 100]]),
-            'labels': tensor([[10, 14, 10, 11, 11, 11, 11, 11]])}]
 
-    test = [{'input_ids': tensor([[100, 864, 19676, 3832, 100, 173, 14203, 100]]),
-             'labels': tensor([[10, 14, 10, 11, 11, 11, 11, 11]])}]
-    train(args, ipt)
-    precision, recall, f1_score = evaluate(args, test)
+    train_data = get_data('data/splits/train.conll', args.model, args.batch_size)
+    test_data = get_data('data/splits/test.conll', args.model, args.batch_size)
+    #
+    # ipt = [{'input_ids': tensor([[100, 864, 19676, 3832, 100, 173, 14203, 100]]),
+    #         'labels': tensor([[10, 14, 10, 11, 11, 11, 11, 11]])}]
+
+    # test = [{'input_ids': tensor([[100, 864, 19676, 3832, 100, 173, 14203, 100]]),
+    #          'labels': tensor([[10, 14, 10, 11, 11, 11, 11, 11]])}]
+
+
+    train(args, train_data)
+    precision, recall, f1_score = evaluate(args, test_data)
     print(precision,recall,f1_score)
